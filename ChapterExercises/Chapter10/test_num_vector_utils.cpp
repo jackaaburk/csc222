964e0d88994b7580d36d13f1d4f39dfd2dabc0aa
jackaaburk <jack.aa.burk@gmail.com>
2024-11-12 12:45:53 -0500

    added more test cases

--- ChapterExercises/Chapter10/test_num_vector_utils.cpp
+++ ChapterExercises/Chapter10/test_num_vector_utils.cpp
@@ -13,4 +13,15 @@ TEST_CASE("Testing render_num_vector") {
 }
 
 TEST_CASE("Testing if vector has other than three elements") {
-    vector<int
+    vector<int> nums = {1, 3, 7, 10};
+    string numstr = render_num_vector(nums);
+    string expected = "1 3 7 10";
+    CHECK(numstr == expected);
+}
+
+TEST_CASE("Testing if vector has 0 elements") {
+    vector<int> nums = {};
+    string numstr = render_num_vector(nums);
+    string expected = "";
+    CHECK(numstr == expected);
+}
