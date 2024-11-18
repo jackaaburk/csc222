4604dfd7ce4eca360ec3fc15954eae630aa46e59
jackaaburk <jack.aa.burk@gmail.com>
2024-11-18 16:39:49 -0500

    started 3, made skeleton of test cases

---
+++ ChapterExercises/Chapter11/test_fraction.cpp
@@ -0,0 +1,24 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <string>
+#include <vector>
+#include "Fraction.h"
+using namespace std;
+
+TEST_CASE("Testing Addition") {
+}
+
+TEST_CASE("Testing Subtraction") {
+}
+
+TEST_CASE("Testing Multiplication") {
+}
+
+TEST_CASE("Testing Division") {
+}
+
+TEST_CASE("Testing Denominator 0") {
+}
+
+TEST_CASE("Testing No Arg Constructor") {
+}
