78a5270fd8c98d378ec2331a3e1a89d77d9c2fa7
jackburkh <jackattack20011@gmail.com>
2024-11-19 22:32:10 -0500

    12 done

--- ChapterExercises/Chapter11/main.cpp
+++ ChapterExercises/Chapter11/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 struct Point {
     double x, y;
-
+    Point operator+(const Point& other);
     Point(double x, double y) {
         this->x = x;
         this->y = y;
     }
 };
+
+Point Point::operator+(const Point& other) {
+    return Point(x + other.x, y + other.y);
+}
 
 int main() {
     Time current_time = {9, 14, 30.0};
@@ -24,6 +28,9 @@ int main() {
 
     Time elapsed_time(3661.5);
     elapsed_time.print();
-
+    Point p1 = Point(3, 4);
+    Point p2 = Point(2, 1);
+    Point p3 = p1 + p2;
+    cout << "p3: (" << p3.x << ", " << p3.y << ")" << endl;
     return 0;
 }
