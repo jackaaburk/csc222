ab82c2747d826c84a23211d22b9e724193534e89
jackaaburk <jack.aa.burk@gmail.com>
2024-11-20 16:18:32 -0500

    did fraction and times, not sure if u want it to return a double or two ints or something. Had trouble initializing the instance variable on return so it would actually print the modified value, anyways I already did my earlier minutes, and I just spent another 40 trying to figure this out

--- ChapterExercises/Chapter11/ChapterExercises/Fraction.cpp
+++ ChapterExercises/Chapter11/ChapterExercises/Fraction.cpp
@@ -7,18 +7,33 @@ struct Fraction {
         numerator = n;
         denominator = d;
     }
+    Fraction() {
+        numerator = 0;
+        denominator = 1;
+    }
     void print();
+    int times(int n, int d);
 
 };
 
-void Fraction::print()
-{
+void Fraction::print() {
     Fraction fraction = *this;
     cout << fraction.numerator << "/" << fraction.denominator << endl;
 }
 
+int Fraction::times(int n, int d) {
+    numerator *= n;
+    denominator *= d;
+    return numerator, denominator;
+}
+
+
 int main() {
-    Fraction current_fraction = {1, 2};
+    Fraction current_fraction(1, 2);
+    Fraction parameterless_fraction;
+    current_fraction.print();
+    parameterless_fraction.print();
+    current_fraction.times(10, 2);
     current_fraction.print();
 
 
