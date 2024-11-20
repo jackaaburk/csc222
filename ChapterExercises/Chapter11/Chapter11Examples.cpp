a3dabf7eb5f31bf66c8be462c58a0df0e32ebfa6
jackburkh <jackattack20011@gmail.com>
2024-11-19 21:09:26 -0500

    4

--- ChapterExercises/Chapter11/Chapter11Examples.cpp
+++ ChapterExercises/Chapter11/Chapter11Examples.cpp
@@ -2,23 +2,37 @@
 using namespace std;
 
 
+
 struct Time {
     int hour, minute;
     double second;
 
     void print();
+    void increment(double secs);
 };
 
 
 void Time::print() {
-    Time time = *this;
-    cout << time.hour << ":" << time.minute << ":" << time.second << endl;
+    cout << hour << ":" << minute << ":" << second << endl;
 }
 
+
+void Time::increment(double secs) {
+    second += secs;
+    while (second >= 60.0) {
+        second -= 60.0;
+        minute += 1;
+    }
+    while (minute >= 60) {
+        minute -= 60;
+        hour += 1;
+    }
+}
 
 
 int main() {
     Time current_time = {9, 14, 30.0};
+    current_time.increment(500.0);
     current_time.print();
     return 0;
 }
