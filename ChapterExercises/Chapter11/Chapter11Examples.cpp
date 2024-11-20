1e8dfbc79105e5683915164f45497836c654f317
jackburkh <jackattack20011@gmail.com>
2024-11-19 21:20:47 -0500

    6

--- ChapterExercises/Chapter11/Chapter11Examples.cpp
+++ ChapterExercises/Chapter11/Chapter11Examples.cpp
@@ -9,6 +9,7 @@ struct Time {
     void print();
     void increment(double secs);
     double convert_to_seconds() const;
+    bool after(const Time& t2) const;
 };
 
 
@@ -19,6 +20,7 @@ void Time::print() {
 
 void Time::increment(double secs) {
     second += secs;
+
     while (second >= 60.0) {
         second -= 60.0;
         minute += 1;
@@ -35,11 +37,24 @@ double Time::convert_to_seconds() const {
 }
 
 
+bool Time::after(const Time& t2) const {
+    if (hour > t2.hour) return true;
+    if (hour < t2.hour) return false;
+    if (minute > t2.minute) return true;
+    if (minute < t2.minute) return false;
+    return (second > t2.second);
+}
+
+
 int main() {
     Time current_time = {9, 14, 30.0};
     current_time.print();
     current_time.increment(500.0);
     current_time.print();
     cout << current_time.convert_to_seconds() << endl;
+    Time done_time = {10, 14, 31.0};
+    if (done_time.after(current_time)) {
+    cout << "The bread will be done after it starts." << endl;
+}
     return 0;
 }
