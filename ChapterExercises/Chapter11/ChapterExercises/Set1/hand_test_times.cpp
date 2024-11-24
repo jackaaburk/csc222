425ad57a9cd8aa5e7e9ab7d9ac35841b64b8b40e
jackaaburk <jack.aa.burk@gmail.com>
2024-11-23 20:23:26 -0500

    added operator to main but not header

--- ChapterExercises/Chapter11/ChapterExercises/Set1/hand_test_times.cpp
+++ ChapterExercises/Chapter11/ChapterExercises/Set1/hand_test_times.cpp
@@ -3,6 +3,17 @@
 #include "Time.h"
 using namespace std;
 
+Time::() : hours(0), minutes(0), seconds(0) {}
+
+Time::Time(int totalSeconds) {
+    hours=(totalSeconds / 3600);
+    minutes=((totalSeconds % 3600) / 60);
+    seconds=(totalSeconds % 60);
+}
+
+ostream& operator<<(ostream& os, const Time& tm) {
+    os << tm.hours << ':' << tm.minutes << ':' << tm.seconds;
+}
 
 int main() {
     Time t(3600 * 4 + 60 * 11 + 22);
