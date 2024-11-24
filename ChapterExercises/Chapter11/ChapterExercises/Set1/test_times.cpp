9987d880fcd352d27141781f45a2f3509f96a36b
jackaaburk <jack.aa.burk@gmail.com>
2024-11-23 19:32:37 -0500

    added new case

--- ChapterExercises/Chapter11/ChapterExercises/Set1/test_times.cpp
+++ ChapterExercises/Chapter11/ChapterExercises/Set1/test_times.cpp
@@ -14,3 +14,10 @@ TEST_CASE("Test can create and render Times") {
     Time t4(7 * 3600 + 11 * 60 + 19);
     CHECK(t4.to_string() == "7:11:19");
 }
+
+TEST_CASE("Test hour-minute and hour-minute-second constructors") {
+    Time t1(5, 37);
+    CHECK(t1.to_string() == "5:37:00");
+    Time t2(7, 2, 11);
+    CHECK(t2.to_string() == "7:02:11");
+}
