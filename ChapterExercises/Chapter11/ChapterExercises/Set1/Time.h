80af6442a39481e5f11afc099b11bb7b5554096f
jackaaburk <jack.aa.burk@gmail.com>
2024-11-23 20:24:53 -0500

    did h file, cant compile

--- ChapterExercises/Chapter11/ChapterExercises/Set1/Time.h
+++ ChapterExercises/Chapter11/ChapterExercises/Set1/Time.h
@@ -12,5 +12,6 @@ struct Time {
     Time(int totalSeconds);
     Time(int h, int m, int s = 0);
     Time operator+(Time const& obj);
+    ostream& operator<<(ostream& os, const Time& tm);
     string to_string() const;
 };
