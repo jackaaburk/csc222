1ac9118610ed3871647647bfab8bd0b700f7c758
jackaaburk <jack.aa.burk@gmail.com>
2024-12-03 12:15:50 -0500

    got vehicle test to render

--- CPrograms/VehicleRacerWithStruct/Vehicle.cpp
+++ CPrograms/VehicleRacerWithStruct/Vehicle.cpp
@@ -3,19 +3,20 @@
 #include <string>
 using namespace std;
 
-Vehicle::Vehicle(int weight, int cylinders) {
-  weight = weight;
-  cylinders = cylinders;
+Vehicle::Vehicle(int w, int c) {
+  weight = w;
+  cylinders = c;
   }
 
-Vehicle::Vehicle(int weight, int cylinders, int num_wheels) {
-  weight = weight;
-  cylinders = cylinders;
-  num_wheels = num_wheels;
+Vehicle::Vehicle(int w, int c, int nw) {
+  weight = w;
+  cylinders = c;
+  num_wheels = nw;
   }
 
 string Vehicle::to_string() {
   string outputstring;
-  outputstring = "This is a car that's " + ::to_string(weight) + "kg with " + ::to_string(cylinders) + " cylinders";
+  outputstring = "This is a vehicle that's " + ::to_string(weight) + "kg with " + ::to_string(cylinders) + " cylinders";
   return outputstring;
 }
+
