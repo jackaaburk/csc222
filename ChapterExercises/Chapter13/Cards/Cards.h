e9255b9f9f1f327e3574986c23cbd2d98be5d780
jackaaburk <jack.aa.burk@gmail.com>
2024-12-19 00:08:37 -0500

    reverted commit

--- ChapterExercises/Chapter13/Cards/Cards.h
+++ ChapterExercises/Chapter13/Cards/Cards.h
@@ -10,10 +10,8 @@ struct Card
 {
     Suit suit;
     Rank rank;
-
     Card();
     Card(int s, int r);
-    
     bool operator==(const Card&) const;
     bool operator>(const Card&) const;
     bool operator<(const Card&) const;
@@ -21,19 +19,4 @@ struct Card
     bool operator<=(const Card&) const;
     bool operator!=(const Card&) const;
     string to_string() const;
-};
-
-struct Deck {
-    vector<Card> cards;
-
-    Deck(int size);
-    Deck();
-
-    void print() const;
-    void shuffle();
-    void sort();
-    Deck subdeck(int l, int h) const;
-    void add_card(const Card& c);
-    Card remove_card();
-    void swap_cards(int i, int j);
 };
