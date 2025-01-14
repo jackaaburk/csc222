cc9f46d4c15340d1c3ad48447f41550610d68bd3
jackaaburk <jack.aa.burk@gmail.com>
2025-01-14 11:57:17 -0500

    starting bigints

---
+++ BigIntProject/test_bigints.cpp
@@ -0,0 +1,31 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+#include <string>
+#include "BigInt.h"
+using namespace std;
+
+TEST_CASE("Test can create and render BigInts") {
+    BigInt bi;
+    CHECK(bi.to_string() == "0");
+    BigInt bi2(42);
+    CHECK(bi2.to_string() == "42");
+    BigInt bi3(-42);
+    CHECK(bi3.to_string() == "-42");
+    BigInt bi4("123456789012345678901234567890");
+    CHECK(bi4.to_string() == "123456789012345678901234567890");
+    BigInt bi5("-923456789012345678901234567890");
+    CHECK(bi5.to_string() == "-923456789012345678901234567890");
+}
+
+TEST_CASE("Test can compare BigInts for equality") {
+    BigInt i1("12345");
+    BigInt i2("54321");
+    BigInt i3("123456");
+    BigInt i4("-654321");
+    BigInt i5("54321");
+    BigInt i6("-54321");
+    CHECK((i2 == i5) == true);
+    CHECK((i1 == i2) == false);
+    CHECK((i1 == i3) == false);
+    CHECK((i2 == i6) == false);
+}
